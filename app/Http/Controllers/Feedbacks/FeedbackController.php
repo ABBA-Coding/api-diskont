53e68e93e228ec1b015916062affbcc049c2ddc6
Shohijahon Axmetov <shohijahonaxmetov@gmail.com>
2023-03-04 11:20:52 +0500

    feedbacks end

--- app/Http/Controllers/Feedbacks/FeedbackController.php
+++ app/Http/Controllers/Feedbacks/FeedbackController.php
@@ -107,24 +107,30 @@ class FeedbackController extends Controller
             'logo' => isset($request->logo) ? $request->logo : null,
         ]);
 
-        $old_images = $feedback->images()->pluck('id')->toArray();
+        $old_images_ids = $feedback->images()->pluck('id')->toArray();
         if(!empty($request->images)) {
-            $result_imgs = [];
+            $request_old_imgs_ids = [];
+            $new_imgs = []; // ["img1", "img2"]
+            // dd($request->images);
             foreach($request->images as $item) {
-                if($item->id != 0) {
-                    $result_imgs;
-                }
-                if(Storage::disk('public')->exists('/uploads/temp/' . explode('/', $item)[count(explode('/', $item)) - 1])) {
-                    $explode_img = explode('/', $item);
-                    Storage::disk('public')->move('/uploads/temp/' . $explode_img[count($explode_img) - 1], '/uploads/feedbacks/' . $explode_img[count($explode_img) - 1]);
-                    Storage::disk('public')->move('/uploads/temp/200/' . $explode_img[count($explode_img) - 1], '/uploads/feedbacks/200/' . $explode_img[count($explode_img) - 1]);
-                    Storage::disk('public')->move('/uploads/temp/600/' . $explode_img[count($explode_img) - 1], '/uploads/feedbacks/600/' . $explode_img[count($explode_img) - 1]);
-                    $imgs[] = $explode_img[count($explode_img) - 1];
+                if($item['id'] == 0) {
+                    if(Storage::disk('public')->exists('/uploads/temp/' . explode('/', $item['img'])[count(explode('/', $item['img'])) - 1])) {
+                        $explode_img = explode('/', $item['img']);
+                        Storage::disk('public')->move('/uploads/temp/' . $explode_img[count($explode_img) - 1], '/uploads/feedbacks/' . $explode_img[count($explode_img) - 1]);
+                        Storage::disk('public')->move('/uploads/temp/200/' . $explode_img[count($explode_img) - 1], '/uploads/feedbacks/200/' . $explode_img[count($explode_img) - 1]);
+                        Storage::disk('public')->move('/uploads/temp/600/' . $explode_img[count($explode_img) - 1], '/uploads/feedbacks/600/' . $explode_img[count($explode_img) - 1]);
+                        $new_imgs[] = $explode_img[count($explode_img) - 1];
+                    }
+                } else {
+                    $request_old_imgs_ids[] = $item['id'];
                 }
             }
+            FeedbackImage::where('feedback_id', $feedback->id)
+                ->whereNotIn('id', $request_old_imgs_ids)
+                ->delete();
 
-            if(isset($imgs) && !empty($imgs)) {
-                foreach($imgs as $item) {
+            if(isset($new_imgs) && !empty($new_imgs)) {
+                foreach($new_imgs as $item) {
                     FeedbackImage::create([ 
                         'feedback_id' => $feedback->id,
                         'img' => $item
