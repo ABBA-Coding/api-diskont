51af7b785419dabb725631003198586495944e78
shohijahonaxmetov <shohijahonaxmetov@gmail.com>
2023-03-21 13:05:51 +0500

    product show end

--- app/Http/Controllers/web/ProductController.php
+++ app/Http/Controllers/web/ProductController.php
@@ -118,20 +118,26 @@ class ProductController extends Controller
         $siblings = $product->info->products;
         foreach($siblings as $sibling) {
             foreach($sibling->attribute_options as $attribute_option) {
-                $attributes[] = $attribute_option->attribute->id;
+                $attributes[] = [
+                    'id' => $attribute_option->attribute->id,
+                    'title' => $attribute_option->attribute->name
+                ];
             }
         }
-        $unique_attributes_ids = array_unique($attributes);
+        // return response($attributes);
+        $attributes = array_unique($attributes, SORT_REGULAR);
+        // return response($attributes);
 
         $result_attributes = [];
         $counter = 0;
-        foreach($unique_attributes_ids as $unique_attributes_id) {
-            $result_attributes[$counter]['id'] = $unique_attributes_id;
-            $result_attributes[$counter]['options'] = Attribute::find($unique_attributes_id)->options->pluck('id')->toArray();
+        foreach($attributes as $attribute) {
+            // return response($attribute);
+            $result_attributes[$counter]['id'] = $attribute['id'];
+            $result_attributes[$counter]['title'] = $attribute['title'];            
+            $result_attributes[$counter]['options'] = Attribute::find($attribute['id'])->options->pluck('id')->toArray();
 
             $counter ++;
         }
-        unset($counter);
         // return response($result_attributes);
 
         /*
@@ -158,226 +164,85 @@ class ProductController extends Controller
                     ...
                 ]
             ]
-        */
+        */       
+
         $res = [];
         $counter = 0;
+
         for($i=0; $i<count($result_attributes); $i++) {
             $attribute_id = $result_attributes[$i]['id'];
             foreach($result_attributes[$i]['options'] as $option) {
+                // return response($current_product_options_ids_with_attribute_keys);
                 $temp_this_product_options = $current_product_options_ids_with_attribute_keys;
                 $temp_this_product_options[$attribute_id] = $option;
-                // var_dump($temp_this_product_options);echo '<br>';
-                // return response($temp_this_product_options);
+                // dd($temp_this_product_options);
                 foreach($real_combinations as $real_combination) {
-                    // return response($temp_this_product_options);
+                    // return response($real_combination);
                     if(empty(array_diff($temp_this_product_options, $real_combination['options']))) {
-                        $res[$counter]['title'] = 'title-' . $counter;
-                        $res[$counter]['options']['title'] = 'option-title-' . $counter;
-                        $res[$counter]['options']['slug'] = $real_combination['slug'];
-                        $res[$counter]['options']['active'] = true;
-                        $res[$counter]['options']['available'] = true;
+                        $res[$i]['title'] = $result_attributes[$i]['title'];
+                        $res[$i]['options'][$counter]['title'] = AttributeOption::find($option)->name;
+                        $res[$i]['options'][$counter]['slug'] = $real_combination['slug'];
+                        // return response($current_product_options_ids);
+                        $res[$i]['options'][$counter]['active'] = empty(array_diff($temp_this_product_options, $current_product_options_ids)) ? true : false;
+                        $res[$i]['options'][$counter]['available'] = true;
+
+                        $counter ++;
+                    }
+                }
+            }
+
+            $counter = 0;
+        }
+        unset($counter);
+        // return response($res);
+
+
+
+        /*
+            qolgan variaciyalarni ham q6wamiz
+        */
+
+        $first_attribute = $result_attributes[0];
+        $temp = [];
+        $counter = count($res[0]['options']);
+
+        $slugs_array = [];
+        foreach($res as $item) {
+            foreach($item['options'] as $option) {
+                $slugs_array[] = $option['slug'];
+            }
+        }
+        $slugs_array = array_unique($slugs_array);
+        // return response($slugs_array);
+
+        if(count($first_attribute['options']) > count($res[0]['options'])) {
+            foreach($this->combs($result_attributes) as $variants) {
+                foreach($real_combinations as $real_combination) {
+                    // return response($real_combination);
+                    if(empty(array_diff($variants, $real_combination['options'])) && !in_array($real_combination['slug'], $slugs_array)) {
+                        $res[0]['title'] = $first_attribute['title'];
+                        $res[0]['options'][$counter]['title'] = AttributeOption::find($real_combination['options'][0])->name;
+                        $res[0]['options'][$counter]['slug'] = $real_combination['slug'];
+                        // return response($current_product_options_ids);
+                        $res[0]['options'][$counter]['active'] = false;
+                        $res[0]['options'][$counter]['available'] = true;
 
                         $counter ++;
                     }
                 }
             }
         }
-
-        foreach($real_combinations as $real_combination) {
-            $res[$counter]['title'] = 'title-' . $counter;
-            $res[$counter]['options']['title'] = 'option-title-' . $counter;
-            $res[$counter]['options']['slug'] = $real_combination['slug'];
-            $res[$counter]['options']['active'] = true;
-            $res[$counter]['options']['available'] = true;
-
-            $counter ++;
-        }
         unset($counter);
         // return response($res);
 
-        /*
-            sdelat massiv s unikalnimi znacheniyami
-        */
-        $temp = [];
-        $unique_res = [];
-        foreach($res as $item) {
-            if(!in_array($item['options']['slug'], $temp)) {
-                $temp[] = $item['options']['slug'];
-                
-                $unique_res[] = $item;
-            }
-        }
-        unset($temp);
-        return response($unique_res);
 
 
 
-
-        return response($product);
+        return response([
+            'product' => $product,
+            'attributes' => $res
+        ]);
     }
-
-    // public function show($slug)
-    // {
-    //     $product = Product::where('slug', $slug)
-    //         // ->with('info', 'info.brand', 'info.category', 'images', 'characteristic_options', 'characteristic_options.characteristic')
-    //         // ->with('info.category.attributes', 'info.category.attributes.options')
-    //         ->with('attribute_options')
-    //         ->first();
-    //     // return response($product);
-
-    //     $current_product_options_ids = $product->attribute_options->pluck('id')->toArray();
-    //     // id options dannogo produkta v formate [attribute_id => option_id, ...]
-    //     $current_product_options_ids_with_attribute_keys = [];
-        
-    //     foreach($current_product_options_ids as $key => $current_product_options_id) {
-    //         $temp = Attribute::whereHas('options', function($q) use ($current_product_options_id) {
-    //             $q->where('id', $current_product_options_id);
-    //         })->first()->id;
-    //         $current_product_options_ids_with_attribute_keys[$temp] = $current_product_options_id;
-    //     }
-    //     // return response($current_product_options_ids_with_attribute_keys);
-
-    //     // vse attributi s optionami
-    //     $attributes = [];
-    //     $attribute_options = [];
-    //     $siblings = $product->info->products;
-    //     foreach($siblings as $sibling) {
-    //         foreach($sibling->attribute_options as $attribute_option) {
-    //             $attributes[] = $attribute_option->attribute->id;
-    //         }
-    //     }
-    //     $unique_attributes_ids = array_unique($attributes);
-
-    //     $result_attributes = [];
-    //     $counter = 0;
-    //     foreach($unique_attributes_ids as $unique_attributes_id) {
-    //         $result_attributes[$counter]['id'] = $unique_attributes_id;
-    //         $result_attributes[$counter]['options'] = Attribute::find($unique_attributes_id)->options->pluck('id')->toArray();
-
-    //         $counter ++;
-    //     }
-    //     unset($counter);
-    //     // return response($result_attributes);
-
-
-
-
-
-    //     $result_arr = [];
-    //     $attributes_count = count($result_attributes);
-        
-    //     for($i=0; $i<$attributes_count; $i++) {
-    //         // $result_arr[$result_attributes[$i]['id']] = $result_attributes[$i]['options'][0];
-    //         $result_arr[] = $result_attributes[$i]['options'][0];
-    //     }
-    //     // return response($result_arr);
-
-
-    //     // for($l=0; $l<count($result_attributes[0]['options']); $l++) {
-    //     //     for($k=0; $k<count($result_attributes[1]['options']); $k++) {
-    //     //         for ($i=0; $i<count($result_attributes[2]['options']); $i++) {
-    //     //             for($j=0; $j<count($result_attributes[3]['options']); $j++) {
-    //     //                 $result_arr[$result_attributes[3]['id']] = $result_attributes[3]['options'][$j];
-    //     //                 dd($result_arr);
-    //     //                 var_dump($result_arr);echo "<br>";
-    //     //             }
-    //     //             $result_arr[$result_attributes[3]['id']] = $result_attributes[3]['options'][0];
-
-    //     //             if($i != count($result_attributes[2]['options']) -1) {
-    //     //                 $result_arr[$result_attributes[2]['id']] = $result_attributes[2]['options'][$i+1];
-    //     //             }
-    //     //         }
-    //     //         $result_arr[$result_attributes[2]['id']] = $result_attributes[2]['options'][0];
-
-    //     //         if($k != count($result_attributes[1]['options']) -1) {
-    //     //             $result_arr[$result_attributes[1]['id']] = $result_attributes[1]['options'][$k+1];
-    //     //         }
-    //     //     }
-    //     //     $result_arr[$result_attributes[1]['id']] = $result_attributes[1]['options'][0];
-
-    //     //     if($l != count($result_attributes[0]['options']) -1) {
-    //     //         $result_arr[$result_attributes[0]['id']] = $result_attributes[0]['options'][$l+1];
-    //     //     }
-    //     // }
-    //     // exit();
-
-
-    //     // for($i1 = 0; $i1 < count($result_attributes[0]['options']); $i1 ++){
-    //     //     for($i2 = 0; $i2 < count($result_attributes[1]['options']); $i2 ++){
-    //     //         for($i3 = 0; $i3 < count($result_attributes[2]['options']); $i3 ++){
-    //     //             for($i4 = 0; $i4 < count($result_attributes[3]['options']); $i4 ++){
-
-    //     //                 $variant = [
-    //     //                     $result_attributes[0]['id'] => $result_attributes[0]['options'][$i1],
-    //     //                     $result_attributes[1]['id'] => $result_attributes[1]['options'][$i2],
-    //     //                     $result_attributes[2]['id'] => $result_attributes[2]['options'][$i3],
-    //     //                     $result_attributes[3]['id'] => $result_attributes[3]['options'][$i4],
-    //     //                 ];
-    //     //                 var_dump($variant);echo "<br>";
-
-    //     //             }
-    //     //         }
-    //     //     }
-    //     // }
-    //     // exit();
-
-
-
-    //     // vse vozmojnie kombinacii produktov
-    //     $real_combinations = [];
-    //     $counter = 0;
-    //     foreach($siblings as $sibling) {
-    //         $real_combinations[$counter]['slug'] = $sibling->slug;
-    //         $real_combinations[$counter]['options'] = $sibling->attribute_options->pluck('id')->toArray();
-
-    //         $counter ++;
-    //     }
-    //     unset($counter);
-    //     return response($real_combinations);
-
-
-    //     foreach($this->combs($result_attributes) as $item) {
-    //         foreach($real_combinations as $real_combination) {
-    //             $temp_arr = array_diff($item, $real_combination['options']);
-    //             // if(empty($temp_arr))     
-    //         }
-    //         var_dump($item); echo '<br>';
-    //     }
-    //     exit();
-
-
-
-
-    //     return response([
-    //         'attributes' => $attributes,
-    //         'product' => $product,
-    //     ]);
-    // }
-
-
-
-
-
-
-
-    // for($i1 = 0; $i1 < count($result_attributes[0]['options']); $i1 ++){
-    //     for($i2 = 0; $i2 < count($result_attributes[1]['options']); $i2 ++){
-    //         for($i3 = 0; $i3 < count($result_attributes[2]['options']); $i3 ++){
-    //             for($i4 = 0; $i4 < count($result_attributes[3]['options']); $i4 ++){
-
-    //                 $variant = [
-    //                     $result_attributes[0]['id'] => $result_attributes[0]['options'][$i1],
-    //                     $result_attributes[1]['id'] => $result_attributes[1]['options'][$i2],
-    //                     $result_attributes[2]['id'] => $result_attributes[2]['options'][$i3],
-    //                     $result_attributes[3]['id'] => $result_attributes[3]['options'][$i4],
-    //                 ];
-    //                 var_dump($variant);echo "<br>";
-
-    //             }
-    //         }
-    //     }
-    // }
-
 
     function combs($arrays) {
         $result = array();
@@ -404,26 +269,5 @@ class ProductController extends Controller
         }
 
         return $result;
-    }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    private function sort_and_redef($arr)
-    {
-        asort($arr);
-        return array_values($arr);
     }
 }
