33b7913d21ae9c03cc3784427be46b93e7417b7d
shohijahonaxmetov <shohijahonaxmetov@gmail.com>
2023-06-24 14:27:47 +0500

    1c in process

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -53,6 +53,11 @@ class RouteServiceProvider extends ServiceProvider
                 ->namespace($this->namespace)
                 ->group(base_path('routes/mobile.php'));
 
+            Route::prefix('c')
+                ->middleware('c')
+                ->namespace($this->namespace)
+                ->group(base_path('routes/c.php'));
+
             Route::middleware('web')
                 ->namespace($this->namespace)
                 ->group(base_path('routes/web.php'));
