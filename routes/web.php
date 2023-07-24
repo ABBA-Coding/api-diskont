25bb81b9c2aa788b54273a4038be0a4d695989a3
Shohijahon Axmetov <shohijahonaxmetov@gmail.com>
2023-07-24 14:11:26 +0500

    payments package install

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,22 @@ Route::get('/', function () {
     return 'API for e-shop NDC.uz';
 });
 
+
+//handle requests from payment system
+Route::any('/handle/{paysys}',function($paysys){
+    (new Goodoneuz\PayUz\PayUz)->driver($paysys)->handle();
+});
+
+//redirect to payment system or payment form
+Route::any('/pay/{paysys}/{key}/{amount}',function($paysys, $key, $amount){
+	$model = Goodoneuz\PayUz\Services\PaymentService::convertKeyToModel($key);
+    $url = request('redirect_url','/'); // redirect url after payment completed
+    $pay_uz = new Goodoneuz\PayUz\PayUz;
+    $pay_uz
+    	->driver($paysys)
+    	->redirect($model, $amount, 860, $url);
+});
+
 // Route::get('iuwebiuerwv', function() {
 // 	$categories = App\Models\Showcase::all();
 // 	foreach ($categories as $value) {
